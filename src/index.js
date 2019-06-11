2ab8327b6a160fbac8959fb9ac21e544a2cd566b
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-11 14:33:31 -0300

    Add mongoose and set mongo cluster connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+const connectionURI =
+  'mongodb+srv://dbUser:dbUser@clusterpostsproject' +
+  '-3jqwe.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(connectionURI, {
+  useNewUrlParser: true
+})
 
 app.get('/', (req, res) => {
-  return res.send(`OK`)
+  return res.send(`Hello ${req.query.name}`)
 })
 
 app.listen(3333)
